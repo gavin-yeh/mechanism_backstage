75cd2f4fd9e6a5bfcf4361bf4cfa2bf74e405f72
twgavin <twgavin@ko-noha.com>
2023-07-22 13:53:32 +0800

    add private staff curve

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">職員姓名：{{ name }}</div>
+
+    <Curve :popup-data="curveData" />
   </div>
+
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Curve from '../staff-product-report/curve/curve.vue'
 
 export default {
   name: 'Dashboard',
+  components: {
+    Curve
+  },
+  data() {
+    return {
+      curveData: { staffId: 0 }
+    }
+  },
   computed: {
     ...mapGetters([
-      'name'
+      'name',
+      'staffId'
     ])
+  },
+  async created() {
+    this.curveData = { staffId: this.staffId }
   }
 }
 </script>
@@ -27,4 +43,11 @@ export default {
     line-height: 46px;
   }
 }
+
+.curve-container {
+  width: 1200px;            // 設定框框的寬度
+  height: 150px;            // 設定框框的高度
+  border: 1px solid #ccc; // 設定框框的邊框樣式
+}
+
 </style>
